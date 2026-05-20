5b5337627739c78dd77983c3fc0eb583b9a5518c
Justin Pyron <justinpyron@gmail.com>
2026-05-20 09:39:50 -0500

    Add five cases

--- evals/datasets/leaderboard.py
+++ evals/datasets/leaderboard.py
@@ -155,6 +155,41 @@ dataset: Dataset[WikiGolfEvalInput, WikiGolfEvalOutput] = Dataset(
                 destination="Tuxedo_Junction",
             ),
         ),
+        Case(
+            name="case_21",
+            inputs=WikiGolfEvalInput(
+                origin="Romer_v._Evans",
+                destination="Maryland_and_Virginia_Rifle_Regiment",
+            ),
+        ),
+        Case(
+            name="case_22",
+            inputs=WikiGolfEvalInput(
+                origin="CSI:_Crime_Scene_Investigation",
+                destination="Baeolophus",
+            ),
+        ),
+        Case(
+            name="case_23",
+            inputs=WikiGolfEvalInput(
+                origin="Woodland",
+                destination="Rutgers_Law_School",
+            ),
+        ),
+        Case(
+            name="case_24",
+            inputs=WikiGolfEvalInput(
+                origin="International_Union_for_Conservation_of_Nature",
+                destination="Early_Christianity",
+            ),
+        ),
+        Case(
+            name="case_25",
+            inputs=WikiGolfEvalInput(
+                origin="Elevation",
+                destination="Australian_English",
+            ),
+        ),
     ],
     evaluators=[
         ReachedDestination(),
